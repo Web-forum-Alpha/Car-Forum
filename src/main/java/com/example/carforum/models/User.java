2f735a766ea49fd68068c9021d9f12974a908e4f
Aleksandar Oynakov <sofia88paradise@gmail.com>
2026-06-28 23:22:01 +0300

    Updated user model

--- src/main/java/com/example/carforum/models/User.java
+++ src/main/java/com/example/carforum/models/User.java
@@ -1,6 +1,7 @@
 package com.example.carforum.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -17,12 +18,14 @@ public class User {
     private String password;
 
     @Column(name = "first_name")
+    @Size(min = 4, max = 32)
     private String firstName;
 
     @Column(name = "last_name")
+    @Size(min = 4, max = 32)
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
 
     @Column(name = "phone_number")
