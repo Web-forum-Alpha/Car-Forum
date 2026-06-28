57e94baf7b7e8f43048251a3b108c18afd6a656d
Aleksandar Oynakov <sofia88paradise@gmail.com>
2026-06-29 00:09:25 +0300

    Create initial User Repository

---
+++ src/main/java/com/example/carforum/repositories/UserRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.example.carforum.repositories;
+
+import com.example.carforum.models.User;
+import jakarta.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UserRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<User> getAll(){
+        return entityManager
+                .createQuery("from User", User.class)
+                .getResultList();
+    }
+
+    @Override
+    public User getById(int id){
+        return entityManager
+                .find(User.class,id);
+    }
+
+    @Override
+    public User getByUsername(String userName){
+        return entityManager.find(User.class,userName);
+    }
+}
